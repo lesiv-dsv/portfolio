4a3e6a3adb20b469da8fad3e0f6c35f0e18126cc
lesiv-dsv <lesivvolodymyr04@gmail.com>
2023-06-07 21:14:49 +0300

    new file:   .gitignore 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.png 	new file:   public/index.html 	new file:   public/manifest.json 	new file:   public/robots.txt 	new file:   src/App.css 	new file:   src/App.js 	new file:   src/App.test.js 	new file:   src/Assets/Projects/blog.png 	new file:   src/Assets/Projects/chatify.png 	new file:   src/Assets/Projects/codeEditor.png 	new file:   src/Assets/Projects/emotion.png 	new file:   src/Assets/Projects/leaf.png 	new file:   src/Assets/Projects/suicide.png 	new file:   src/Assets/about.png 	new file:   src/Assets/avatar.svg 	new file:   src/Assets/home-bg.jpg 	new file:   src/Assets/home-main.svg 	new file:   src/Assets/logo.png 	new file:   src/Assets/pre.svg 	new file:   src/Assets/resume_volodymyr_lesiv.pdf 	new file:   src/components/About/About.js 	new file:   src/components/About/AboutCard.js 	new file:   src/components/About/Github.js 	new file:   src/components/About/Techstack.js 	new file:   src/components/About/Toolstack.js 	new file:   src/components/Footer.js 	new file:   src/components/Home/Home.js 	new file:   src/components/Home/Home2.js 	new file:   src/components/Home/Type.js 	new file:   src/components/Navbar.js 	new file:   src/components/Particle.js 	new file:   src/components/Pre.js 	new file:   src/components/Resume/ResumeNew.js 	new file:   src/components/ScrollToTop.js 	new file:   src/index.css 	new file:   src/index.js 	new file:   src/reportWebVitals.js 	new file:   src/setupTests.js 	new file:   src/style.css

---
+++ src/components/Home/Home2.js
@@ -0,0 +1,97 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/avatar.svg";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+            </h1>
+            <p className="home-about-body">
+              Hello, and welcome to my portfolio!
+              <br />
+              <br />I am thrilled to have this opportunity to showcase my expertise and experience as a 
+              <i>
+                <b className="purple"> Data Analyst. </b>
+              </i>
+              <br />
+              <br />
+              My passion for &nbsp;
+              <i>
+                <b className="purple">exploring </b>  and <b className="purple">extracting meaningful insights &nbsp;</b>
+              </i>
+              from data has driven me to excel in this field.
+              <br />
+              <br />
+              With a strong foundation in statistical 
+              with <b className="purple">analysis, programming,</b> and
+              <i>
+                <b className="purple">
+                  data visualization,
+                </b>
+              </i>
+              &nbsp; I strive to leverage data-driven solutions to address complex business challenges.
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>FIND ME ON</h1>
+            <p>
+              Feel free to <span className="purple">connect </span>with me
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/lesiv-dsv"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/volodymyrlesiv/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.instagram.com/lesiv.w/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <AiFillInstagram />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default Home2;
